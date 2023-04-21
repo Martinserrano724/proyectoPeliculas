4233d5054ddbb996d2e76ba9ff0dcf87ac5c9ce8
Martinserrano724 <martinserrano724@gmail.com>
2023-04-21 17:49:02 -0300

    Creacion de la logica  de administrador

--- js/administrador.js
+++ js/administrador.js
@@ -1,5 +1,31 @@
 //como utilizar codigo de otro archivo JS en un archivo nuevo
 import Pelicula from "./classPelicula.js";
 
+const btnEditar=document.getElementById('btnEditar');
+const btnAgregar=document.getElementById('btnAgregar');
+btnEditar.addEventListener('click',crearPeli)
+btnAgregar.addEventListener('click',mostrarModalPelicula)
+const formPelicula = document.getElementById('formAdministrarPelicula');
+formPelicula.addEventListener('submit',cargarPelicula);
+
 let nuevaPeli = new Pelicula('super mario','algo','url','aventura',2023,'2hr','EEUU','-')
 console.log(nuevaPeli);
+//instanciamos un objeto sobre el archivo de modal de botstrap
+const modalPelicula = new bootstrap.Modal(document.getElementById('modalAgregar'))
+
+
+
+function crearPeli(){
+
+}
+function mostrarModalPelicula(){
+//modalPelicula nos sirve para crear un intancia de un modal desde JS
+modalPelicula.show();
+}
+function cargarPelicula(){
+    //nos sirve para para poder usar un formulario
+    e.preventDefault()
+    
+    //para ocultar la ventana modal
+    modalPelicula.hide();
+}
