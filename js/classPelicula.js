767d64534c54bcbc22e7f1f9a63fae25b15ee4c2
Martinserrano724 <martinserrano724@gmail.com>
2023-04-21 16:24:57 -0300

    Creacion de los getter y setter de la clase peliculas

---
+++ js/classPelicula.js
@@ -0,0 +1,105 @@
+class Pelicula{
+    //atrivutos privados
+    #codigo;
+    #titulo;
+    #descripcion;
+    #imagen;
+    #genero;
+    #anio;
+    #duracion;
+    #pais;
+    #reparto;
+    #estado;
+    constructor(titulo,descripcion,imagen,genero,anio,duracion,pais,reparto){
+        this.#codigo=0;
+        this.#titulo=titulo;
+        this.#descripcion=descripcion;
+        this.#imagen=imagen;
+        this.#genero=genero;
+        this.#anio=anio;
+        this.#duracion=duracion;
+        this.#pais=pais;
+        this.#reparto=reparto;
+        this.#estado=false;
+    }
+
+// getter y setter para el atributo codigo
+getCodigo(){
+    return this.#codigo;
+  }
+  setCodigo(codigo) {
+    this.#codigo = codigo;
+  }
+
+  // getter y setter para el atributo titulo
+  getTitulo() {
+    return this.#titulo;
+  }
+  setTitulo(titulo) {
+    this.#titulo = titulo;
+  }
+
+  // getter y setter para el atributo descripcion
+  getDescripcion() {
+    return this.#descripcion;
+  }
+  setDescripcion(descripcion) {
+    this.#descripcion = descripcion;
+  }
+
+  // getter y setter para el atributo imagen
+  getImagen() {
+    return this.#imagen;
+  }
+  setImagen(imagen) {
+    this.#imagen = imagen;
+  }
+
+  // getter y setter para el atributo genero
+  getGenero() {
+    return this.#genero;
+  }
+  setGenero(genero) {
+    this.#genero = genero;
+  }
+
+  // getter y setter para el atributo anio
+  getAnio() {
+    return this.#anio;
+  }
+  setAnio(anio) {
+    this.#anio = anio;
+  }
+
+  // getter y setter para el atributo duracion
+  getDuracion() {
+    return this.#duracion;
+  }
+  setDuracion(duracion) {
+    this.#duracion = duracion;
+  }
+
+  // getter y setter para el atributo pais
+  getPais() {
+    return this.#pais;
+  }
+  setPais(pais) {
+    this.#pais = pais;
+  }
+
+  // getter y setter para el atributo reparto
+  getReparto() {
+    return this.#reparto;
+  }
+  setReparto(reparto) {
+    this.#reparto = reparto;
+  }
+
+  // getter y setter para el atributo estado
+  getEstado() {
+    return this.#estado;
+  }
+  setEstado(estado) {
+    this.#estado = estado;
+  }
+}
